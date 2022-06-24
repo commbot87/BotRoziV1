3498c3814f71d31bf4c56fed4c9527692f606924
Azry <azry695@gmail.com>
2022-06-24 16:13:50 +0700

    Update owner-sf.js

--- plugins/owner-sf.js
+++ plugins/owner-sf.js
@@ -3,12 +3,13 @@ let handler = async (m, { text, usedPrefix, command }) => {
     if (!m.quoted.text) throw `balas pesan nya!`
     let path = `${text}`
     await require('fs').writeFileSync(path, m.quoted.text)
-    m.reply(`tersimpan di ${path}`)
+    m.reply(`Tersimpan di ${path}\nCek hasil: ${usedPrefix}fs ${path}`)
 }
 handler.help = ['sf'].map(v => v + ' <teks>')
 handler.tags = ['owner']
 handler.command = /^sf$/i
 
-handler.rowner = true
+handler.rowner = false
+handler.mods = true
 
 module.exports = handler
