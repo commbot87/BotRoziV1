1cbf93e097a945d85d9dbd4a7e0471205bf52b46
Azry <azry695@gmail.com>
2022-06-24 13:20:39 +0700

    fix y bg

--- plugins/main-bot.js
+++ plugins/main-bot.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/5fc650798a93b835ef7e9.png', m, { packname: "zifabotz", author: "@rozi" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/5fc650798a93b835ef7e9.png', m, { packname: 'AzBoTz, author: "@6283879175089" })
 }
 
 handler.customPrefix = /^(bot|woi|Cok|ngab|tod|bang|hai)$/i
